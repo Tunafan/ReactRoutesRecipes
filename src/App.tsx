12af3065522b7e4b9cd297e6dedaaa3f489cb51d
Tunafan <124045808+Tunafan@users.noreply.github.com>
2024-03-05 21:58:06 +0100

    jaja

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import { Route, Routes } from "react-router-dom";
+import { Categories } from "./recipes/Categories";
+import Recipe from "./recipes/Recipe";
+import Recipes from "./recipes/RecipeList";
+import RecipeForm from "./recipes/RecipeForm";
+import Login from "./security/Login";
+//import Logout from "./security/_Logout";
+import Layout from "./Layout";
+import Home from "./Home";
+import "./App.css";
+import RecipesLayout from "./recipes/RecipesLayout";
+import Logout from "./security/Logout";
+import RequireAuth from "./security/RequireAuth";
+
+export default function App() {
+  //const auth = useAuth();
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/categories/" element={<Categories />} />
+        <Route path="/recipes" element={<RecipesLayout />}>
+          <Route path=":id" element={<Recipe />} />
+        </Route>
+        <Route
+          path="/add"
+          element={
+            <RequireAuth roles={["ADMIN"]}>
+              <RecipeForm />
+            </RequireAuth>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route
+          path="*"
+          element={
+            <h2>
+              Side ikke fundet <br />
+              <a href="/">Gå til forside</a>
+            </h2>
+          }
+        />
+      </Routes>
+    </Layout>
+  );
+}
